c954c676597aa1bab752f7109b19446294114506
dlhandsome <505441575@qq.com>
2020-03-31 19:52:57 +0800

    fix(core): fixed options

--- packages/core/src/libs/api/init.js
+++ packages/core/src/libs/api/init.js
@@ -1,18 +1,22 @@
 import { use } from './use.js';
 import ErrorPlugin from '@we-debug/plugin-error';
 import RouterPlugin from '@we-debug/plugin-router';
+import merge from 'deepmerge';
 
 /**
  * 初始化方法
  * @param {*} options
  */
 export function init(options = {}) {
-  if (!options.plugins) {
-    options.plugins = {
-      error: true,
-      router: true
-    };
-  }
+  options = merge(
+    {
+      plugin: {
+        error: true,
+        router: true
+      }
+    },
+    options
+  );
 
   if (options.plugins.error) use.call(this, ErrorPlugin, options.plugins.error.options || {});
   if (options.plugins.router) use.call(this, RouterPlugin, options.plugins.router.options || {});
