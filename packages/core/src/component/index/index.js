3879e281dd7788ace9a440c201f8f939eccd1782
dlhandsome <505441575@qq.com>
2020-04-02 11:31:15 +0800

    fix(core): remove launcher code inside component

--- packages/core/src/component/index/index.js
+++ packages/core/src/component/index/index.js
@@ -2,24 +2,6 @@ import Debug from '../../libs/index';
 
 const store = Debug.store;
 
-const debugBadge = Debug.createBadge({
-  key: 'debug',
-  value: '调试胶囊',
-  show: true,
-  draggable: true,
-  position: {
-    right: 10,
-    bottom: 20
-  },
-  handler: {
-    bindTap() {
-      store.event.emit('debug:mask:show-modal');
-    }
-  }
-});
-
-Debug.addBadge(debugBadge);
-
 Component({
   properties: {
     config: {
@@ -42,7 +24,6 @@ Component({
   data: {
     sys: store.sys.get(),
     rules: store.rules.get(),
-    badges: store.badges.get(),
-    debugBadge
+    badges: store.badges.get()
   }
 });
