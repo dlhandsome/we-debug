8e1db40e3902a114732bf0e33eb4128247499ad3
dlhandsome <505441575@qq.com>
2020-04-13 16:29:33 +0800

    feat(core): added network plugin

--- packages/core/src/libs/api/init.js
+++ packages/core/src/libs/api/init.js
@@ -2,6 +2,7 @@ import { use } from './use.js';
 import ErrorPlugin from '@we-debug/plugin-error';
 import RouterPlugin from '@we-debug/plugin-router';
 import LauncherPlugin from '@we-debug/plugin-launcher';
+import networkPlugin from '@we-debug/plugin-network';
 import merge from 'deepmerge';
 
 /**
@@ -21,6 +22,7 @@ export function init(options = {}) {
   );
 
   if (options.launcher) use.call(this, LauncherPlugin, options.launcher || {});
+  if (options.plugin.network) use.call(this, networkPlugin, options.plugin.network || {});
   if (options.plugin.error) use.call(this, ErrorPlugin, options.plugin.error || {});
   if (options.plugin.router) use.call(this, RouterPlugin, options.plugin.router || {});
 
