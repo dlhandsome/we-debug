f1d45c05213f7c6864426453aa5008ce011b5efb
dlhandsome <awsomeduan@gmail.com>
2020-12-08 14:23:28 +0800

    feat(runtime/core): support async event

--- packages/runtime/core/test/index.test.js
+++ packages/runtime/core/test/index.test.js
@@ -1,5 +1,5 @@
 require('./wx');
-const Debug = require('../src/libs/index').default;
+const Debug = require('../src/index').default;
 
 test('test event', done => {
   const badge = Debug.createBadge();
@@ -11,5 +11,24 @@ test('test event', done => {
 
   badge.emit({
     show: true
+  });
+});
+
+test('test event emit result', done => {
+  function returnPromiseA(v) {
+    return Promise.resolve(v);
+  }
+
+  function returnPromiseB(v) {
+    return Promise.resolve(++v);
+  }
+
+  Debug.event.on('debug:test-promise', returnPromiseA);
+  Debug.event.on('debug:test-promise', returnPromiseB);
+
+  const result = Debug.event.emit('debug:test-promise', 1);
+  Promise.all(result).then(res => {
+    expect(res).toStrictEqual([1, 2]);
+    done();
   });
 });
