5ae475590f7d32a1a149ec62765105b89eb2d3fe
Sail <505441575@qq.com>
2024-01-16 09:04:16 +0000

    chore: support typescript

--- gulpfile.js
+++ gulpfile.js
@@ -8,23 +8,34 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const replace = require('gulp-replace');
 const builds = require('./scripts/config');
+const gulpTs = require('gulp-typescript');
 
 const genCopyTaskName = suffix => 'copy:' + suffix;
 const genScriptTaskName = suffix => 'script:' + suffix;
+const genTsTaskName = suffix => 'ts:' + suffix;
 const genLessTaskName = suffix => 'less:' + suffix;
 
 const buildTasks = builds
   .map(p => {
     const copyTask = genCopyTaskName(p.name);
     const scriptTask = genScriptTaskName(p.name);
+    const tsTask = genTsTaskName(p.name);
     const lessTask = genLessTaskName(p.name);
     const pkgPath = path.resolve(__dirname, p.pkg);
 
-    gulp.task(copyTask, () => {
+    p.copySrc && gulp.task(copyTask, () => {
       return gulp.src(p.copySrc, p.option).pipe(gulp.dest(p.dest));
     });
 
-    gulp.task(scriptTask, () => {
+    p.tsSrc && gulp.task(tsTask, () => {
+      return gulp
+        .src(p.tsSrc, p.option)
+        .pipe(gulpTs.createProject('tsconfig.json')())
+        .pipe(replace('__VERSION__', JSON.stringify(require(pkgPath).version)))
+        .pipe(gulp.dest(p.dest));
+    });
+
+    p.scriptsSrc && gulp.task(scriptTask, () => {
       return gulp
         .src(p.scriptsSrc, p.option)
         .pipe(replace('__VERSION__', JSON.stringify(require(pkgPath).version)))
@@ -37,7 +48,7 @@ const buildTasks = builds
         .pipe(gulp.dest(p.dest));
     });
 
-    gulp.task(lessTask, () => {
+    p.lessSrc && gulp.task(lessTask, () => {
       return gulp
         .src(p.lessSrc, p.option)
         .pipe(
@@ -55,7 +66,12 @@ const buildTasks = builds
         .pipe(gulp.dest(p.dest));
     });
 
-    return [copyTask, scriptTask, lessTask];
+    return [
+      ...(p.copySrc ? [copyTask] : []),
+      ...(p.scriptsSrc ? [scriptTask] : []),
+      ...(p.tsSrc ? [tsTask] : []),
+      ...(p.lessSrc ? [lessTask] : []),
+    ];
   })
   .reduce((p, n) => p.concat(n), []);
 
@@ -63,11 +79,13 @@ gulp.task('build-watch', () => {
   builds.forEach(p => {
     const copyTask = genCopyTaskName(p.name);
     const scriptTask = genScriptTaskName(p.name);
+    const tsTask = genTsTaskName(p.name);
     const lessTask = genLessTaskName(p.name);
 
-    gulp.watch(p.copySrc, gulp.series(copyTask));
-    gulp.watch(p.scriptsSrc, gulp.series(scriptTask));
-    gulp.watch(p.lessSrc, gulp.series(lessTask));
+    p.copySrc && gulp.watch(p.copySrc, gulp.series(copyTask));
+    p.tsSrc && gulp.watch(p.tsSrc, gulp.series(tsTask));
+    p.scriptsSrc && gulp.watch(p.scriptsSrc, gulp.series(scriptTask));
+    p.lessSrc && gulp.watch(p.lessSrc, gulp.series(lessTask));
   });
 });
 
